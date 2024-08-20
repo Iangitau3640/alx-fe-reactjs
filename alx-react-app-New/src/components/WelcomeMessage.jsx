fc2d3c5d7f0ca22d21381ccfbd84ade228639144
Iangitau3640 <164074867+Iangitau3640@users.noreply.github.com>
2024-08-20 12:38:10 +0300

    m

---
+++ alx-react-app-New/src/components/WelcomeMessage.jsx
@@ -0,0 +1,17 @@
+import WelcomeMessage from './WelcomeMessage';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage'; // Import the new component
+
+function WelcomeMessage() {
+  return(
+    <div>
+      <h1> “Hello everyone, I am learning React at ALX!”</h1>
+      <p> "I am learning about JSX!"</p>
+    </div>
+  );
+}
+
+export default App;
